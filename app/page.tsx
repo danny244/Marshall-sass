10ef2a37487489277effc17d4f4af2482a10fffc
danny <idowudaniel289@gmail.com>
2024-03-06 23:29:49 +0100

    Last Push

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,6 @@ export default async function Home() {
         return redirect('/dashboard')
     }
 
-
-    
-    
     return (
         <section className="flex items-center justify-center bg-background h-[90vh]">
             <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
